56e8a9fe55f2f3d489fd5a6a56c95cbdb8f81061
timac11 <timac11@bk.ru>
2020-01-26 01:51:10 +0300

    change productinfo component

--- src/components/productInfo/productInfo.jsx
+++ src/components/productInfo/productInfo.jsx
@@ -2,43 +2,142 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import MenuItem from "@material-ui/core/MenuItem/MenuItem";
+import MaterialTable from "material-table";
+import FormLabel from "@material-ui/core/FormLabel/FormLabel";
+import RadioGroup from "@material-ui/core/RadioGroup/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel/FormControlLabel";
+import Radio from "@material-ui/core/Radio/Radio";
+
+
+const currencies = [
+    {
+        value: 'Канцелярские товары',
+        label: 'Канцелярские товары',
+    }
+];
 
 export default function ProductInfo() {
+    const [currency, setCurrency] = React.useState('Канцулярские товары');
+    const [state, setState] = React.useState({
+        columns: [
+            {title: 'Название', field: 'name'},
+            {title: 'Количество', field: 'quantity', type: 'numeric'},
+            {title: 'Цена', field: 'price', type: 'numeric'}
+        ],
+        data: [
+            {name: 'Product1', quantity: 1, price: 1987},
+            {name: 'Product2', quantity: 1, price: 1987},
+        ],
+    });
+
+    const handleChange = event => {
+        setCurrency(event.target.value);
+    };
+
     return (
-        <React.Fragment>
-            <Typography variant="h6" gutterBottom>
-                Параметры проекта
+        <div style={{maxHeight: "90vh"}}>
+            <Typography variant="h4" gutterBottom>
+                Информация о закупке
             </Typography>
             <Grid container spacing={3}>
                 <Grid item xs={12}>
                     <TextField
-                        required
+                        select
+                        label="Категория"
+                        value={currency}
+                        onChange={handleChange}
+                        helperText="Выберите категорию товара"
+                        fullWidth
+                    >
+                        {currencies.map(option => (
+                            <MenuItem key={option.value} value={option.value}>
+                                {option.label}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </Grid>
+                <Grid item xs={12}>
+                    <MaterialTable
+                        localization={{
+                            header: {
+                                actions: ""
+                            },
+                            body: {
+                                emptyDataSourceMessage: 'Нет продуктов',
+                                addTooltip: "Добавить",
+                                deleteTooltip: "Удалить",
+                                editTooltip: "Изменить",
+                                editRow: {
+                                    deleteText: "Удалить товар?"
+                                }
+                            }
+                        }}
+                        options={{
+                            rowStyle: {
+                                backgroundColor: '#EEE',
+                            },
+                            search: false
+                        }}
+                        searchable={false}
+                        title="Список товаров"
+                        columns={state.columns}
+                        data={state.data}
+                        editable={{
+                            onRowAdd: newData =>
+                                new Promise(resolve => {
+                                    setTimeout(() => {
+                                        resolve();
+                                        setState(prevState => {
+                                            const data = [...prevState.data];
+                                            data.push(newData);
+                                            return {...prevState, data};
+                                        });
+                                    }, 600);
+                                }),
+                            onRowUpdate: (newData, oldData) =>
+                                new Promise(resolve => {
+                                    setTimeout(() => {
+                                        resolve();
+                                        if (oldData) {
+                                            setState(prevState => {
+                                                const data = [...prevState.data];
+                                                data[data.indexOf(oldData)] = newData;
+                                                return {...prevState, data};
+                                            });
+                                        }
+                                    }, 600);
+                                }),
+                            onRowDelete: oldData =>
+                                new Promise(resolve => {
+                                    setTimeout(() => {
+                                        resolve();
+                                        setState(prevState => {
+                                            const data = [...prevState.data];
+                                            data.splice(data.indexOf(oldData), 1);
+                                            return {...prevState, data};
+                                        });
+                                    }, 600);
+                                }),
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
                         id="name"
                         name="name"
-                        label="Название"
+                        label="Место доставки"
                         fullWidth
-                        autoComplete="fname"
                     />
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField
-                        required
-                        id="description"
-                        name="description"
-                        label="Описание проекта"
-                        fullWidth
-                        autoComplete="billing address-line1"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        id="dop"
-                        name="dop"
-                        label="Дополнительная информация"
-                        fullWidth
-                    />
+                    <FormLabel component="legend">Тип закупки</FormLabel>
+                    <RadioGroup>
+                        <FormControlLabel value="one" control={<Radio />} label="Единоразовая" />
+                        <FormControlLabel value="two" control={<Radio />} label="Регулярная" />
+                    </RadioGroup>
                 </Grid>
             </Grid>
-        </React.Fragment>
+        </div>
     );
 }
